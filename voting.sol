5664cac013be6df42a562bb9f812a8dea13b823f
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-08-16 13:00:10 +0300

    Update voting.sol

--- voting.sol
+++ voting.sol
@@ -1,9 +1,40 @@
 pragma solidity ^0.8.19;
 
 contract Voting{
-    struct Voter;
+    struct Voter{
     bool voted;
     uint vote;
 }
 
-mapping(address => )
+mapping(address => Voter ) public voters;
+uint public candidateCount;
+mapping(uint => address) public candidates;
+
+event Voted(address indexed voter, uint indexed candidate);
+
+constructor(uint _candidateCount) public{
+    candidateCount = _candidateCount;
+    for(uint i = 0; i < candidateCount; i++){
+        candidates[i] = address(0);
+    }
+}
+
+function vote(uint candidate) public{
+    require(!voters[msg.sender].voted);
+    voters[msg.sender].vote = candidate;
+    emit Voted(msg.sender, candidate);
+
+}
+
+function getWinner() public view returns (address){
+    uint maxVoters = 0;
+    address winner = address(0);
+    for(uint i = 0; i < candidateCount; i++){
+        if(voters[i].vote > maxVotes){
+            maxVotes = voters[i].vote;
+            winner = candidates[i];
+        }
+    }
+    return winner
+  } 
+}
