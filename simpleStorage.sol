5155a3da2de0e513a1f867b3d68f613081ed59f0
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-08-19 17:07:28 +0300

    Update simpleStorage.sol

--- simpleStorage.sol
+++ simpleStorage.sol
@@ -4,6 +4,11 @@ contract simpleStorage{
 
     uint256 favouriteNo;
 
+    struct People{
+        uint256 favouriteNo;
+        string name;
+    }
+
     function store(uint256 _favouriteNo) public{
         favouriteNo = _favouriteNo;
     }
