86314693e09db3a57aeb00541aa13a3b1840f334
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-08-19 17:14:17 +0300

    Update structExample.sol

--- structExample.sol
+++ structExample.sol
@@ -11,8 +11,5 @@ contract structExample{
     function addPerson(string memory _name, uint _age) public{
         people.push(Person(_name,_age));
     }
-    function logMessage() public {
-    // Log the message "Hello, world!" to the blockchain
-    log("Hello, world!");
-}
+    
 }
