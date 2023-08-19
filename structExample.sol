28b80773deed4ed0b9214f94e8c711326e800430
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-08-19 16:59:14 +0300

    Update structExample.sol

--- structExample.sol
+++ structExample.sol
@@ -5,4 +5,10 @@ contract structExample{
         string name;
         uint age;
     }
+
+    Person[] public people;
+
+    function addPerson(string memory _name, uint _age) public{
+        people.push(Person(_name,_age));
+    }
 }
