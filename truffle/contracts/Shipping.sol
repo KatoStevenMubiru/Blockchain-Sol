c5aa632e4ebb95ce1ab8f10f6df7d69758546c17
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-08-23 10:39:27 +0300

    Update Shipping.sol

--- truffle/contracts/Shipping.sol
+++ truffle/contracts/Shipping.sol
@@ -2,8 +2,14 @@ pragma solidity ^0.8.0;
 
 contract Shipping{
  
- enum ShippingStatus = {Pending, Shipped, Delivered};
+ enum ShippingStatus {Pending, Shipped, Delivered};
+
+ ShippingStatus private status;
+
+ event LogNewAlert(string description);
 
  
+
+
 
 }
