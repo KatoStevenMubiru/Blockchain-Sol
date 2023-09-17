4f6594a4a3704828e3de439562a99bff5c6dcbad
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-09-17 06:36:48 +0300

    Update modifier2.sol

--- modifiers/modifier2.sol
+++ modifiers/modifier2.sol
@@ -3,4 +3,13 @@ pragma solidity ^0.8.0
 contact Moddy{
     uint public bal;
     address public owner
+
+    constructor(){
+        owner = msg.sender;
+        balance = msg.value;
+
+    }
+    function deposit() public payable{
+        require(msg.sender == owner, "You are the only owner")
+    }
 }
