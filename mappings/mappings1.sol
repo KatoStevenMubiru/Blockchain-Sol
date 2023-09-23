53f0513e09992d5e6a8a786ed66b234c0de13b04
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-09-23 05:56:08 +0300

    Update mappings1.sol

--- mappings/mappings1.sol
+++ mappings/mappings1.sol
@@ -6,11 +6,11 @@ contract Map1{
    /* mapping(uint => bool) map;*/
    mapping(uint => mapping(uint => int)) map;
       
-function addItem(uint key, bool value ) public{
-    map[key] = value;
+function addItem(uint key , uint key2, int value ) public{
+    map[key][key2] = value;
 }
-function getKey(uint key) public view returns(bool){
-    return map[key];
+function getKey(uint key, uint key2) public view returns(int){
+    return map[key][key2];
 
 }
 
