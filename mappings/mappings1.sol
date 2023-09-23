1bf68143a5a0cf9b7625af9f9179e9d91c11c4df
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-09-23 05:36:01 +0300

    Update mappings1.sol

--- mappings/mappings1.sol
+++ mappings/mappings1.sol
@@ -5,6 +5,8 @@ pragma solidity >=0.7.0 <0.9.0;
 contract Map1{
     mapping(uint => bool) map;
       
-function addItem(uint key, bool value ) public
+function addItem(uint key, bool value ) public{
+    
+}
 
 }
