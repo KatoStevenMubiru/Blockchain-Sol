ddefa088ab73f5ee3290a650d4812a4642ec0049
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-11-04 18:11:05 +0300

    Create simpleStorage.sol

---
+++ CryptoChatbot/simpleStorage.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.8.0;
+
+contract simpleStorage{
+
+    uint256 favouriteNo;
+
+    struct People{
+        uint256 favouriteNo;
+        string name;
+    }
+
+    function store(uint256 _favouriteNo) public{
+        favouriteNo = _favouriteNo;
+    }
+
+    function retrieve() public   view returns(uint256){
+              
+              return favouriteNo;
+    }
+}
