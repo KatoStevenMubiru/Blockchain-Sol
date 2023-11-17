688aae55802bbf5d554f752e7ff5e161141baf5d
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-11-17 12:24:05 +0300

    fix login styles

--- StabilityAIAdvert/advertmaker/src/components/pages/login.jsx
+++ StabilityAIAdvert/advertmaker/src/components/pages/login.jsx
@@ -2,27 +2,7 @@
 import { useNavigate } from "react-router-dom";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { auth } from "../firebase/firebase"; // Path to your firebase.js file
-import styled from "styled-components";
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  background: linear-gradient(to right, #4b6cb7, #182848);
-  color: white;
-`;
-
-const Button = styled.button`
-  margin-top: 20px;
-  padding: 10px 20px;
-  border: none;
-  border-radius: 5px;
-  background-color: #61dafb;
-  color: black;
-  cursor: pointer;
-`;
+import { Button, Container } from "../styles/loginStyle";
 
 function LoginPage() {
   const navigate = useNavigate();
---
+++ StabilityAIAdvert/advertmaker/src/components/styles/loginStyle.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background: linear-gradient(to right, #4b6cb7, #182848);
+  color: white;
+`;
+
+export const Button = styled.button`
+  margin-top: 20px;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  background-color: #61dafb;
+  color: black;
+  cursor: pointer;
+`;
